25e2b1552df094c1c55fa70f35d231b4cf93fe9e
Daniel <daniel.cuzco@gmail.com>
2022-07-11 14:15:03 -0400

    added event listener click

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+const margaritaHeader = document.getElementById("margarita-header")
 fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 .then(response => response.json())
-.then(margaritaObject => { margaritaObject.drinks.strDrinksThumb.forEach(
-    addImage(image)
-)
-function addImage(image) {
+.then(margaritaObject => margaritaObject.drinks.forEach(addImage))
+
+function addImage(margarita) { 
+    const margaritaImage = document.createElement("img")
+    margaritaImage.src = margarita.strDrinkThumb
+
+    margaritaImage.addEventListener("click", function () {
+        console.log("you clicked me")
+    })
     
-}
+    margaritaHeader.append(margaritaImage)
+
 
     
-    
-})
+}
 
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
