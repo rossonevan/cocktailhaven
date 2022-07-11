9af377a05460f25dcf6dc4df940671cf6aff05dd
Daniel <daniel.cuzco@gmail.com>
2022-07-11 12:53:04 -0400

    fetch

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+.then(response => response.json())
+.then(margaritaObject => { margaritaObject.drinks.strDrinksThumb.forEach(
+    addImage(image)
+)
+function addImage(image) {
+    
+}
+
+    
+    
+})
+
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
+// document.querySelector("body").appendChild(h2);
