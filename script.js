88d68d9a6ed781168fcea5b969d0cd73899feeca
Daniel <daniel.cuzco@gmail.com>
2022-07-11 15:21:18 -0400

    added click and display

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const margaritaHeader = document.getElementById("margarita-header")
+ const margaritaDisplay = document.querySelector(".margarita-image")
+ console.log(margaritaDisplay)
+
+
 fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 .then(response => response.json())
 .then(margaritaObject => margaritaObject.drinks.forEach(addImage))
@@ -7,15 +11,26 @@ function addImage(margarita) {
     const margaritaImage = document.createElement("img")
     margaritaImage.src = margarita.strDrinkThumb
 
-    margaritaImage.addEventListener("click", function () {
-        console.log("you clicked me")
-    })
+
+   margaritaImage.addEventListener("click", function () {
+        displayDetails(margarita)
+        
+   })
+        
+        
+   margaritaHeader.append(margaritaImage)
     
-    margaritaHeader.append(margaritaImage)
+ }
+        
+function displayDetails(margarita) {
+    margaritaDisplay.src = margarita.strDrinkThumb
+
+     }
+    
 
 
     
-}
+
 
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
